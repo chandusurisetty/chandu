f82cd68f8a0309b88f385ca1e008242e1e96de89
Chandu Surisetty <68140710+chandusurisetty@users.noreply.github.com>
2024-07-28 10:17:48 +0530

    Update script.js
    added whoami

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".myname", {
-    strings: ["Chandu;", "Programmer;"],
+    strings: ["#WhoamI?", "//I'm Chandu;", "&lt;I'm Programmer;&sol;&gt;"],
     typeSpeed: 120,
     backSpeed: 120,
     smartBackspace: true,
