a02886a96abff00965583a3c73e634f95cc6aca7
Chandu Surisetty <68140710+chandusurisetty@users.noreply.github.com>
2024-07-28 11:49:29 +0530

    Update script.js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".myname", {
-    strings: ["#WhoamI?", "//I'm Chandu;", "&lt;I'm Programmer;&sol;&gt;"],
+    strings: ["<span class='red-text'>#WhoamI?</span>", "//I'm Chandu;", "&lt;I'm Programmer;&sol;&gt;"],
     typeSpeed: 120,
     backSpeed: 120,
     smartBackspace: true,
