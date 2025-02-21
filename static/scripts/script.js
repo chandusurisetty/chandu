767b9642ac84a9706d8f8b8845811bb5417485a3
Chandu Surisetty <68140710+chandusurisetty@users.noreply.github.com>
2025-02-21 22:42:32 +0530

    tabba changes

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,11 @@
+function closeWelcomeDisplay() {
+    document.getElementById('welcome-display').style.display = 'none';
+}
+
 var typed = new Typed(".myname", {
     strings: ["<span class='red-text'>#WhoamI?</span>", "//I'm Chandu;", "&lt;I'm Programmer;&sol;&gt;"],
     typeSpeed: 120,
     backSpeed: 120,
     smartBackspace: true,
     loop: true
-})
+});
